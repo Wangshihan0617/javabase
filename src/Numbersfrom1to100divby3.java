6d86337abc5f955abdeffa1f388d9624d0f54aa4
YourName <your@email.com>
2026-06-11 17:49:24 +0800

    添加1~100被3整除案例代码

--- src/Numbersfrom1to100divby3.java
+++ src/Numbersfrom1to100divby3.java
@@ -1,2 +1,11 @@
 public class Numbersfrom1to100divby3 {
+    public static void main(String[] args) {
+        System.out.println("1到100之间能被3整除的数字：");
+        
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
 }
